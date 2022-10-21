608589b8debb96ab1dfbd7d23e75904dd31725c1
Mohamed Khairoun <mkhairou@c1r6p5.1337.ma>
2022-10-21 11:28:02 +0100

    69

---
+++ ft_atoi.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkhairou <mkhairou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/29 15:04:24 by mkhairou          #+#    #+#             */
+/*   Updated: 2022/10/17 17:53:52 by mkhairou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	res;
+
+	i = 0;
+	res = 0;
+	sign = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-')
+	{
+		sign *= -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str && str[i] >= 48 && str[i] <= 57)
+	{
+		res = res * 10;
+		res = res + str[i] - 48;
+		i++;
+	}
+	return (res * sign);
+}
